144a93ec7d247a131b14c814d38c038e7ae3527e
Raynald Mirville <raynald.mirville@gmail.com>
2019-08-06 16:24:26 -0400

    StageLoaderService: Added array and stage property type validation of stage details data

--- src/app/stage-comparison/stage-loader.service.spec.ts
+++ src/app/stage-comparison/stage-loader.service.spec.ts
@@ -1,8 +1,6 @@
 import { async, fakeAsync, tick } from '@angular/core/testing';
 import { asyncData, asyncError } from '../testing/async-observable-helpers';
 
-import * as ERRORS from '../error/error';
-
 import { StageLoaderService } from './stage-loader.service';
 
 import * as STAGE_LIST from './spec/stage-list';
@@ -32,12 +30,14 @@ describe('StageLoaderService', () => {
       let expectedStages = STAGES.PRINCESS_PEACH_CASTLE;
 
       httpClientSpy.get.and.returnValues(asyncData(stageList), asyncData(stageDetails));
+      /**/console.log(`SPEC - stageDetails type: ${typeof stageDetails}`);
+      /**/console.log(`SPEC - stageDetails length: ${stageDetails.length}`);
 
-      let actualStagesObs = service.loadStages();
+      let actualStages$ = service.loadStages();
       /**///console.log('SPEC - obtained observable from service.loadStages()');
 
-      actualStagesObs.subscribe((actualStages) => {
-        /**///console.log('SPEC - actualStages: ' + JSON.stringify(actualStages));
+      actualStages$.subscribe((actualStages) => {
+        /**///console.log(`SPEC - actualStages: ${JSON.stringify(actualStages)}`);
         expect(actualStages.length).toBe(1);
         expect(actualStages).toEqual(expectedStages);
         expect(httpClientSpy.get.calls.count()).toBe(2);
@@ -50,29 +50,59 @@ describe('StageLoaderService', () => {
 
       let stageListBad = STAGE_LIST.BAD_DATA;
       let stageDetails = STAGE_DETAILS.PRINCESS_PEACH_CASTLE;
-      let expectedStages = STAGES.PRINCESS_PEACH_CASTLE;
-      let actualStagesObsArr = [];
+      let actualStages$Arr = [];
 
       for (let i = 0; i < stageListBad.length; i++) {
         httpClientSpy.get.and.returnValues(asyncData(stageListBad[i]), asyncData(stageDetails));
 
         /**///console.log('SPEC - loading stages');
-        actualStagesObsArr[i] = service.loadStages();
-        /**///console.log('SPEC - actualStagesObs: ' + actualStagesObsArr[i]);
-        actualStagesObsArr[i].subscribe({
-          next: (actualStages) => {
+        actualStages$Arr[i] = service.loadStages();
+        /**///console.log(`SPEC - actualStages$: ${actualStages$Arr[i]}`);
+        actualStages$Arr[i].subscribe({
+          next: () => {
             /**///console.log('SPEC - got data somehow');
             fail(`loadStages() did not encounter TypeError`);
           },
           error: (error) => {
-            /**///console.log('SPEC - Error: ' + error);
+            /**///console.log(`SPEC - Error: ${error}`);
             expect(error instanceof TypeError).toBeTruthy(`error instanceof TypeError`);
           }
         });
       }
     }));
     
-    it(`should reject stage details data that isn't of type StageDetails[]`);
+    it(`should reject stage details data that isn't of type StageDetails[]`, fakeAsync(() => {
+      /**/console.log(`SPEC === CHECK STAGE DETAILS DATA TYPE ===`);
+
+      let stageList = STAGE_LIST.PRINCESS_PEACH_CASTLE;
+      let stageDetailsBad = STAGE_DETAILS.BAD_DATA;
+      let actualStages$Arr = [];
+
+      for (let [problem, detailsDataBad] of Object.entries(stageDetailsBad)) {
+
+        /**///console.log('SPEC - setting httpClientSpy');
+        httpClientSpy.get.and.returnValues(asyncData(stageList), asyncData(detailsDataBad));
+        
+        /**///console.log('SPEC - stageDetailsBad type: ' + typeof detailsDataBad);
+
+        /**///console.log('SPEC - loading stages');
+        actualStages$Arr[problem] = service.loadStages();
+        /**///console.log('SPEC - actualStages$: ' + actualStages$Arr[problem]);
+        actualStages$Arr[problem].subscribe({
+          next: () => {
+            /**///console.log('SPEC - got data somehow');
+            fail(`loadStages() did not encounter TypeError`);
+          },
+          error: (error) => {
+            /**///console.log(`SPEC - ${error}`);
+            expect(error instanceof TypeError).toBeTruthy(`error instanceof TypeError`);
+          }
+        });
+        /**///console.log('SPEC - tick started');
+        tick();
+        /**///console.log('SPEC - tick finished');
+      }
+    }));
 
     afterEach(() => {
     });
--- src/app/stage-comparison/stage-loader.service.ts
+++ src/app/stage-comparison/stage-loader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface StageSummary {
   name: string;
@@ -32,79 +33,94 @@ const API_STAGE_DETAILS_PREFIX: string = '/stages/';
 })
 export class StageLoaderService {
 
-  constructor( private _http: HttpClient ) { }
+  constructor( private _http: HttpClient ) {
+  }
 
   loadStages(): Observable<Stage[]> {
-    /**///console.log('StageLoaderService::loadStages() called.');
-    let stagesObs: Observable<Stage[]> = new Observable((observer) => {
+    /**///console.log('  StageLoaderService::loadStages()');
+    let stages$: Observable<Stage[]> = new Observable((observer) => {
 
       let stages: Stage[] = [];
 
       const { next, error } = observer;
 
       // get stage summary data
-      let summariesObs: Observable<StageSummary[]> = this._http.get<StageSummary[]>(API_URL + API_STAGE_LIST_PATH);
-      /**///console.log('  * created summariesObs');
+      let stagesHttp$: Observable<StageSummary[]> = this._http.get<StageSummary[]>(API_URL + API_STAGE_LIST_PATH);
+      /**///console.log(`    * created stagesHttp$: ${stagesHttp$}`);
 
-      summariesObs.subscribe((summaries) => {
-        /**///console.log('  * summariesObs emitted: ' + JSON.stringify(summaries));
-        /**///console.log('  * summariesObs emitted');
-        /**///console.log('stages: ' + JSON.stringify(stages));
+      stagesHttp$.subscribe((summaries) => {
+        /**///console.log(`    * stagesHttp$ emitted: ${JSON.stringify(summaries)}`);
+        /**///console.log('    * stagesHttp$ emitted');
+        /**///console.log(`stages: ${JSON.stringify(stages)}`);
 
         // subscribe to _getStageDetails, providing summaries
-        let stageObs: Observable<Stage> = this._getStageDetails(summaries);
-        stageObs.subscribe({
+        let stage$: Observable<Stage> = this._getStageDetails(summaries);
+        stage$.subscribe({
           next(stage) {
-            /**///console.log('    + summariesObs.subscribe - stageObs emitted: ' + JSON.stringify(stage));
-            /**///console.log('    + summariesObs.subscribe - stageObs emitted');
+            /**///console.log(`      + stagesHttp$.subscribe - stage$ emitted: ${JSON.stringify(stage)}`);
+            /**///console.log('      + stagesHttp$.subscribe - stage$ emitted');
             stages.push(stage);
           },
 
           error(e) {
-            /**///console.log('    + summariesObs.subscribe - error occurred');
+            /**///console.log('      + stagesHttp$.subscribe - error occurred');
             observer.error(e);
           },
           complete() {
-            /**///console.log('    + summariesObs.subscribe - stageObs complete');
+            /**///console.log('      + stagesHttp$.subscribe - stage$ complete');
             observer.next(stages);
           }
         });
       });
-      /**///console.log('  * subscribed to summariesObs');
+      /**///console.log('    * subscribed to stagesHttp$');
     });
 
-    /**///console.log('  * returning stagesObs');
-    return stagesObs;
+    /**///console.log('    * returning stages$');
+    return stages$;
   }
 
   _getStageDetails(summaries: StageSummary[]): Observable<Stage> {
-    /**///console.log('StageLoaderService::_getStageDetails() called.');
-    /**///console.log('  * summaries passed in: ' + JSON.stringify(summaries));
+    /**///console.log('  StageLoaderService::_getStageDetails()');
+    /**///console.log(`    * summaries passed in: ${JSON.stringify(summaries)}`);
 
-    let detailsObs = new Observable<Stage>((observer) => {
-      /**///console.log('  * detailsObs executing');
+    let details$ = new Observable<Stage>((observer) => {
+      /**///console.log('    * details$ executing');
       let stages: Stage[] = [];
       
-      /**///console.log('  * summaries isArray: ' + Array.isArray(summaries));
+      /**///console.log(`    * summaries isArray: ${Array.isArray(summaries)}`);
       if (!Array.isArray(summaries)) {
         throw new TypeError('The stage summary data fetched was not an array');
       }
-      
+      /**///console.log(`      + summaries.length: ${summaries.length}`);
       // for each summary in summaries
       for (let i = 0; i < summaries.length; i++) {
-        /**///console.log('    + detailsObs - summary: ' + JSON.stringify(summaries[i]));
-        /**///console.log('    + checking summary type');
+        /**///console.log(`      + details$ - summary: ${JSON.stringify(summaries[i])}`);
+        /**///console.log('      + checking summary type');
         if (!this._isStageSummary(summaries[i])) {
           /**///console.log('      = throwing TypeError');
           throw new TypeError('The stage summary data fetched was not of type StageSummary[]');
         }
         let url = API_URL + API_STAGE_DETAILS_PREFIX + summaries[i].name + API_STAGE_DETAILS_PATH;
 
-        /**///console.log('    + retrieving details from url: ' + url);
+        /**///console.log(`      + retrieving details from url: ${url}`);
         // retrieve json
-        this._http.get<StageDetails[]>(url).subscribe((details) => {
-          /**///console.log('    + detailsObs - retrieved details: ' + JSON.stringify(details));
-          /**///console.log('    + detailsObs - retrieved details');
+        let stageDetailsHttp$ = this._http.get<StageDetails[]>(url);
+        /**///console.log('      + stageDetailsHttp$: ' + stageDetailsHttp$);
+        stageDetailsHttp$.subscribe((details) => {
+          /**///console.log(`      + details$ - retrieved details: ${JSON.stringify(details)}`);
+          /**///console.log('      + details$ - retrieved details');
+          if (!Array.isArray(details)) {
+            /**///console.log('      + throwing array TypeError');
+            observer.error(new TypeError(`The ${summaries[i].name} stage details data fetched was not an array`));
+            return;
+          }
+          details.forEach((phase) => {
+            if (!this._isStageDetails(phase)) {
+              /**///console.log('      + throwing property TypeError');
+              observer.error(new TypeError(`The ${summaries[i].name} stage details data fetched was not of type StageDetails[]`));
+              return;
+            }
+          });
           stages[i] = {
             "name": summaries[i].name,
             "gameName": summaries[i].gameName,
@@ -120,12 +136,12 @@ export class StageLoaderService {
 
     });
 
-    return detailsObs;
+    return details$;
   }
 
   _isStageSummary(stage): stage is StageSummary {
-    /**///console.log('StageLoaderService::_isStage() called');
-    /**///console.log('  * stage: ' + JSON.stringify(stage));
+    /**///console.log('  StageLoaderService::_isStageSummary()');
+    /**///console.log(`    * stage: ${JSON.stringify(stage)}`);
     if (typeof stage.name !== "string") {
       return false;
     } else if (typeof stage.gameName !== "string") {
@@ -133,7 +149,20 @@ export class StageLoaderService {
     } else if (typeof stage.Type !== "number") {
       return false;
     }
-    /**///console.log('  * returning true');
+    /**///console.log('    * returning true');
+    return true;
+  }
+
+  _isStageDetails(phase): phase is StageDetails {
+    /**///console.log('  StageLoaderService::_isStageDetails()');
+    /**///console.log(`    * details: ${JSON.stringify(phase)}`);
+    if (typeof phase.stage !== "string") {
+      return false;
+    }
+    return true;
+  }
+
+  _isStageCollisions(collisions): collisions is StageCollisions {
     return true;
   }
 }

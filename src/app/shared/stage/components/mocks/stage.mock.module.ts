5d4a4355eb06133508b5a3418e6003c3edadf018
Raynald Mirville <raynald.mirville@gmail.com>
2020-05-01 16:42:59 -0400

    Build Error Fix - Registered mock components to mock module

--- src/app/shared/stage/components/mocks/stage.mock.module.ts
+++ src/app/shared/stage/components/mocks/stage.mock.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 
 import { StageComparatorGraphMockComponent } from './stage-comparator-graph.mock.component';
+import { StageComparatorGraphHostMockComponent } from './stage-comparator-graph-host.mock.component';
 import { StageComparatorNumberTableMockComponent } from './stage-comparator-number-table.mock.component';
+import { StageComparatorNumberTableHostMockComponent } from './stage-comparator-number-table-host.mock.component';
 import { StageComparatorTextTableMockComponent } from './stage-comparator-text-table.mock.component';
+import { StageComparatorTextTableHostMockComponent } from './stage-comparator-text-table-host.mock.component';
 import { StageSelectHostMockComponent } from './stage-select-host.mock.component';
 import { StageSelectMockComponent } from './stage-select.mock.component';
 
 @NgModule({
   declarations: [
     StageComparatorGraphMockComponent,
+    StageComparatorGraphHostMockComponent,
     StageComparatorNumberTableMockComponent,
+    StageComparatorNumberTableHostMockComponent,
     StageComparatorTextTableMockComponent,
+    StageComparatorTextTableHostMockComponent,
     StageSelectHostMockComponent,
     StageSelectMockComponent
   ]

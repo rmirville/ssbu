9e96d3e727708e70fb3dc246dd6624cc591ca661
Raynald Mirville <raynaldm@pm.me>
2020-06-18 09:07:52 -0400

    Moved Credits into footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,6 @@ const ROUTES: Routes = [
     path: 'stage-comparison',
     loadChildren: () => import('./modules/stage-comparator/stage-comparator.module').then(m => m.StageComparatorModule)
   },
-  {
-    path: 'credits',
-    loadChildren: () => import('./modules/credits/credits.module').then(m => m.CreditsModule)
-  },
   { path: '',
     redirectTo: 'stage-comparison',
     pathMatch: 'full'

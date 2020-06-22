6b13ec5d4d6b15053f08a4f801161a8bb7a39195
Raynald Mirville <raynaldm@pm.me>
2020-06-22 09:49:41 -0400

    Added IE 11 compatibility

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,6 +20,10 @@
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'core-js';
+import 'classlist';
+import 'typedarray';
+import 'formdata-polyfill';
 
 /**
  * Web Animations `@angular/platform-browser/animations`

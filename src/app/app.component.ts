2a5ef2b6a52b5ca1452349b549d683f560cdd2b7
Raynald Mirville <raynaldm@pm.me>
2020-07-02 13:48:50 -0400

    Accessibility: Skip Link - Added skip link - Tweaked focus outline styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+const BASE_HREF: string = '/ssbu';
 
 @Component({
   selector: 'ssbu-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Super Smash Bros. Ultimate Tools';
+  ariaSkipPath = BASE_HREF + '/#main';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      if( !this.router.url.endsWith('#main') ) {
+        this.ariaSkipPath = BASE_HREF + this.router.url + '#main';
+      }
+      else {
+        this.ariaSkipPath = BASE_HREF + this.router.url;
+      }
+    });
+  }
 }

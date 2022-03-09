82ee8084b92d0cff07ac7139084e2f1f0227c067
Raynald Mirville <raynaldm@pm.me>
2022-03-09 15:52:53 -0500

    hotfix: switch ComparatorResolver combo operator

--- src/app/modules/stage-comparator/stage-comparator-resolver.service.ts
+++ src/app/modules/stage-comparator/stage-comparator-resolver.service.ts
@@ -5,8 +5,8 @@ import {
   ActivatedRouteSnapshot
 } from '@angular/router';
 
-import { Observable, forkJoin } from 'rxjs';
-import { concatMap, map } from 'rxjs/operators';
+import { Observable, zip } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 import { StageLoaderService } from '../../shared/stage/services/stage-loader.service';
 import { StageClassificationsService } from '../../shared/stage/services/stage-classifications.service';
@@ -30,18 +30,13 @@ export class StageComparatorResolverService implements Resolve<StageComparatorRo
   constructor(private sls: StageLoaderService, private sds: StageDimensionsService, private scs: StageClassificationsService, private spms: StagePieceMapService, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<StageComparatorRouteData> {
-    /**/
-    // console.log('StageComparatorResolverService::resolve()');
     const stageData$ = new Observable<StageComparatorRouteData>(observer => {
-      /**/
-      // console.log(`  * observer at start: ${JSON.stringify(Object.keys(observer))}`);
       let stageData: StageComparatorRouteData = {stages: null, dimensionsFull: null, stageClassifications: null};
-      
-      /**/
+
       const stageCalculations$ = this.spms.getMaps('stageComparator')
         .pipe(
           // use piece maps to get raw stage data, then save piece maps for the next operation
-          concatMap(pieceMaps => this.sls.loadStages('include', pieceMaps.map(stage => stage.lvd))
+          switchMap(pieceMaps => this.sls.loadStages('include', pieceMaps.map(stage => stage.lvd))
             .pipe(
               map(stages => {
                 // add raw stage data to the route data
@@ -51,18 +46,17 @@ export class StageComparatorResolverService implements Resolve<StageComparatorRo
             )
           ),
           // use piece maps and raw stage data to calculate dimensions and get classification data
-          concatMap(rawData => {
+          switchMap(rawData => {
             const stagesBasic = rawData.stages.map(stage => { return { name: stage.name, gameName: stage.gameName }; });
-            return forkJoin(
+            return zip(
               this.sds.getDimensionsFull(rawData.stages, rawData.pieceMaps),
               this.scs.classifyStages(stagesBasic)
-            ).pipe(
-              map(([dimensionsFull, stageClassifications]) => {
-                return { dimensionsFull, stageClassifications };
-              })
             );
+          }),
+          map(([dimensionsFull, stageClassifications]) => {
+            return { dimensionsFull, stageClassifications };
           })
-      );
+        );
       // add final calculations to the route data
       stageCalculations$.subscribe(stageCalculations => {
         stageData.dimensionsFull = stageCalculations.dimensionsFull;
